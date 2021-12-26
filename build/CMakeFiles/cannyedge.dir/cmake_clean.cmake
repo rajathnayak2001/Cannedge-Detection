de6dc32e57670a93bd30389009f491bb4000144a
rajathnayak2001 <rajath.nayak.2001@gmail.com>
2021-12-26 14:44:48 +0530

    second commit

---
+++ build/CMakeFiles/cannyedge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cannyedge.dir/main.cpp.o"
+  "cannyedge"
+  "cannyedge.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cannyedge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rajath/Desktop/opencv-cannyedge
+# Build directory: /home/rajath/Desktop/opencv-cannyedge/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
